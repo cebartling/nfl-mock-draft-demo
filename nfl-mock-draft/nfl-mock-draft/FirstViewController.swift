dcbf946dcf59ddf8ec61d120e14d6ca1567e14c6
Christopher Bartling <chris.bartling@gmail.com>
2016-12-25 23:27:29 -0600

    Added TableView. 

--- nfl-mock-draft/nfl-mock-draft/FirstViewController.swift
+++ nfl-mock-draft/nfl-mock-draft/FirstViewController.swift
@@ -7,22 +7,38 @@
 //
 
 import UIKit
-//import FirebaseDatabase
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-//    let ref = FIRDatabase.database().reference(withPath: "mock-drafts")
-
+    private let Array: NSArray = ["First","Second","Third"]
+    private var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        self.view.addSubview(myTableView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Num: \(indexPath.row)")
+        print("Value: \(Array[indexPath.row])")
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(Array[indexPath.row])"
+        return cell
+    }
 }
-
