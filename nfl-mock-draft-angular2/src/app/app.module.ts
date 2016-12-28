e3e068ef13637bb5f0c1b91373a06c12bf6a224f
Christopher Bartling <chris.bartling@gmail.com>
2016-12-27 20:44:28 -0600

    Wired up Firebase.

--- nfl-mock-draft-angular2/src/app/app.module.ts
+++ nfl-mock-draft-angular2/src/app/app.module.ts
@@ -1,20 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {AppComponent} from './app.component';
+import { AngularFireModule } from 'angularfire2';
 
-import { AppComponent } from './app.component';
+export const firebaseConfig = {
+    apiKey: "AIzaSyARFUq6069wW_8VHGryWuVxOMqfsETMGw4",
+    authDomain: "nfl-mock-draft-demo.firebaseapp.com",
+    databaseURL: "https://nfl-mock-draft-demo.firebaseio.com",
+    storageBucket: "nfl-mock-draft-demo.appspot.com",
+    messagingSenderId: "414476418606"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AngularFireModule.initializeApp(firebaseConfig)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
